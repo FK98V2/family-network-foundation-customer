bee0dbbd5e97ccee32aac6fb54968ec78a00a97e
FK98V2 <wishira2021.coth@gmail.com>
2025-07-22 23:21:03 +0700

    fix: Correct spacing in announcement label within BoxSlide component for improved readability.

--- app/_components/BoxSlide.tsx
+++ app/_components/BoxSlide.tsx
@@ -80,7 +80,7 @@ export default function BoxSlide({ items, showIntroduce = false }: Props) {
             >
               <Grid size={12}>
                 <Typography variant='h2' whiteSpace='pre-line'>
-                  {showIntroduce && index === 0 ? '' : 'ข่าวสาร'}:{' '}
+                  {showIntroduce && index === 0 ? '' : 'ข่าวสาร:'}{' '}
                   {announcement?.title}
                   {announcement?.createdAt && (
                     <TimeShow times={announcement.createdAt} />
